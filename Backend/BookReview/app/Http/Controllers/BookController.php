0665d39e74a269f2218b45a9600855bf7a987bf0
ShineLynn <sla2482005@gmail.com>
2025-03-28 22:27:42 +0630

    backend api

---
+++ Backend/BookReview/app/Http/Controllers/BookController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json($books);
+    }
+
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'title' => 'required|string',
+            'author' => 'required|string',
+            'path' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $book = Book::create($fields);
+        return response()->json($book, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
